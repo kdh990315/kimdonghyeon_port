ed951adcb4e5a87276637ff1fc1fabc2f79bb8dd
kdh990315 <104114383+kdh990315@users.noreply.github.com>
2023-09-09 02:24:07 +0900

    9.9

--- kimdonghyeon_port/src/components/ProjectView.vue
+++ kimdonghyeon_port/src/components/ProjectView.vue
@@ -3,8 +3,22 @@
 		<div class="project_container">
 			<div class="project_title">
 				<h3>project</h3>
-				<p>나의 결과물</p>
 				<div class="line"></div>
+			</div>
+
+			<div class="project_main">
+				<article class="project_content" v-for="(port, index) in projectInfo" :key="index">
+					<span>{{ index + 1 }}.</span>
+					<div class="project_info">
+						<img :src="port.src" />
+						<p class="project_mainText">{{ port.mainText }}</p>
+						<p class="project_subText">{{ port.subText }}</p>
+						<div class="project_btn_box">
+							<a :href="port.siteURL">site view</a>
+							<a :href="port.codeURL">code view</a>
+						</div>
+					</div>
+				</article>
 			</div>
 		</div>
 	</section>
@@ -12,7 +26,40 @@
 
 <script>
 export default {
-
+	data() {
+		return {
+			projectInfo: [
+				{
+					src: "http://placehold.it/500X300",
+					mainText: "main 더미텍스트입니다.",
+					subText: "sub 더미 텍스트입니다. html 시멘틱태그를 이용하여 가장 기본적인 웹페이지를 만들었습니다.",
+					siteURL: "#",
+					codeURL: "#",
+				},
+				{
+					src: "http://placehold.it/500X300",
+					mainText: "main 더미텍스트입니다.",
+					subText: "suv 더미텍스트입니다.",
+					siteURL: "#",
+					codeURL: "#",
+				},
+				{
+					src: "http://placehold.it/500X300",
+					mainText: "main 더미텍스트입니다.",
+					subText: "suv 더미텍스트입니다.",
+					siteURL: "#",
+					codeURL: "#",
+				},
+				{
+					src: "http://placehold.it/500X300",
+					mainText: "main 더미텍스트입니다.",
+					subText: "suv 더미텍스트입니다.",
+					siteURL: "#",
+					codeURL: "#",
+				},
+			],
+		}
+	}
 }
 </script>
 
@@ -25,22 +72,114 @@ export default {
 	margin-top: 300px;
 
 	.project_title {
+		position: sticky;
+		top: 130px;
+		padding-bottom: 25px;
 
 		h3 {
 			@include title-text;
 		}
 
-		p {
-			font-size: 2vw;
-			padding: 1rem 0;
-		}
-
 		.line {
-			width: 300px;
+			width: 35%;
 			height: 2px;
 			background-color: #fff;
 			border-radius: 50%;
 		}
+	}
+	
+	.project_main {
+		.project_content {
+			width: 100%;
+			padding-bottom: 50px;
+			position: sticky;
+
+
+			span {
+				font-size: 5vw;
+				font-weight: 900;
+				padding-left: 30px;
+			}
+
+			.project_info {
+				position: sticky;
+				
+				@include c-center;
+
+				img {
+					margin-bottom: 20px;
+				}
+
+				p {
+					color: black;
+				}
+
+				.project_mainText {
+					font-size: 1.5rem;
+					font-weight: 900;
+					padding: 5px 0;
+				}
+
+				.project_subText {
+					font-size: 1rem;
+					text-align: center;
+					padding-top: 5px;
+				}
+
+				.project_btn_box {
+					width: 300px;
+					margin-top: 50px;
+
+					@include center-sb;
+
+					a {
+						display: block;
+						font-size: 1.2rem;
+						color: black;
+						padding: 8px 15px;
+						background-color: #ccc;
+					}
+				}
+			}
+
+			&:nth-of-type(1) {
+				background-color: #ffe4c4;
+				top: 255px;
+
+				@media (max-width: 800px) {
+					top: 193px;
+				}
+			}
+			&:nth-of-type(2) {
+				background-color: #e7ceb0;
+				top: 275px;
+
+				@media (max-width: 800px) {
+					top: 213px;
+				}
+			}
+			&:nth-of-type(3) {
+				background-color: #c5b095;
+				top: 295px;
+				
+				@media (max-width: 800px) {
+					top: 233px;
+				}
+			}
+			&:nth-of-type(4) {
+				background-color: #8f806e;
+				top: 315px;
+				
+				@media (max-width: 800px) {
+					top: 253px;
+				}
+			}
+		}
+	}
+
+	@media (max-width: 800px) {
+		width: 100%;
+		margin: 150px auto 0 auto;
 	}
 }
 </style>
--- kimdonghyeon_port/src/components/ScriptView.vue
+++ kimdonghyeon_port/src/components/ScriptView.vue
@@ -1,3 +1,15 @@
 <template>
-	script
+	<section id="script"></section>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#script {
+	height: 100vh;
+}
+</style>
